7eaab23370129414daaf401706e1470af367368e
Judro <juliusdrodofsky@outlook.de>
2022-12-28 21:38:37 +0100

    keep menu entries after leaving the menu

--- src/custom_game.c
+++ src/custom_game.c
@@ -9,9 +9,10 @@ typedef struct CGMenu {
   char cursor_position;
 } CustomGameMenu;
 
-void cm_print(CustomGameMenu *menu, unsigned terminal_width,
-              unsigned terminal_height) {
-  int lml = (terminal_width - 23) / 2 + 1;
+CustomGameMenu custom_game_menu = {40, 10, 40, 0};
+
+void cm_print(unsigned terminal_width, unsigned terminal_height) {
+  int lml = (terminal_width - 21) / 2 + 1;
   char *lm = malloc(lml * sizeof(char));
   for (int i = 0; i < lml; i++) {
     lm[i] = ' ';
@@ -20,49 +21,49 @@ void cm_print(CustomGameMenu *menu, unsigned terminal_width,
   for (int i = 0; i < (terminal_height - 10) / 2; i++) {
     printw("\n");
   }
-  printw("%s┌─────────────────────┐\n", lm);
-  if (menu->cursor_position == 0) {
-    printw("%s│  > width  = <%3d >  │\n", lm, menu->width);
+  printw("%s┌───────────────────┐\n", lm);
+  if (custom_game_menu.cursor_position == 0) {
+    printw("%s│  width  = <%3d >  │\n", lm, custom_game_menu.width);
   } else {
-    printw("%s│    width  =  %3d    │\n", lm, menu->width);
+    printw("%s│  width  =  %3d    │\n", lm, custom_game_menu.width);
   }
-  if (menu->cursor_position == 1) {
-    printw("%s│  > height = <%3d >  │\n", lm, menu->height);
+  if (custom_game_menu.cursor_position == 1) {
+    printw("%s│  height = <%3d >  │\n", lm, custom_game_menu.height);
   } else {
-    printw("%s│    height =  %3d    │\n", lm, menu->height);
+    printw("%s│  height =  %3d    │\n", lm, custom_game_menu.height);
   }
-  if (menu->cursor_position == 2) {
-    printw("%s│  > mines  = <%3d >  │\n", lm, menu->mines);
+  if (custom_game_menu.cursor_position == 2) {
+    printw("%s│  mines  = <%3d >  │\n", lm, custom_game_menu.mines);
   } else {
-    printw("%s│    mines  =  %3d    │\n", lm, menu->mines);
+    printw("%s│  mines  =  %3d    │\n", lm, custom_game_menu.mines);
   }
-  printw("%s└─────────────────────┘\n", lm);
+  printw("%s└───────────────────┘\n", lm);
   free(lm);
 }
-char custom_game_control(CustomGameMenu *menu) {
+char custom_game_control() {
   char ch = getch();
   switch (ch) {
   case 'j':
   case 0x42:
-    if (menu->cursor_position + 1 < 3)
-      menu->cursor_position += 1;
+    if (custom_game_menu.cursor_position + 1 < 3)
+      custom_game_menu.cursor_position += 1;
     break;
   case 'k':
   case 0x41:
-    if (menu->cursor_position > 0)
-      menu->cursor_position -= 1;
+    if (custom_game_menu.cursor_position > 0)
+      custom_game_menu.cursor_position -= 1;
     break;
   case 0x43:
   case 'l':
-    switch (menu->cursor_position) {
+    switch (custom_game_menu.cursor_position) {
     case 0:
-      menu->width++;
+      custom_game_menu.width++;
       break;
     case 1:
-      menu->height++;
+      custom_game_menu.height++;
       break;
     case 2:
-      menu->mines++;
+      custom_game_menu.mines++;
       break;
     default:
       break;
@@ -70,21 +71,21 @@ char custom_game_control(CustomGameMenu *menu) {
     break;
   case 0x44:
   case 'h':
-    switch (menu->cursor_position) {
+    switch (custom_game_menu.cursor_position) {
     case 0:
-      menu->width--;
-      if (menu->width > 10000)
-        menu->width = 0;
+      custom_game_menu.width--;
+      if (custom_game_menu.width > 10000)
+        custom_game_menu.width = 0;
       break;
     case 1:
-      menu->height--;
-      if (menu->height > 10000)
-        menu->height = 0;
+      custom_game_menu.height--;
+      if (custom_game_menu.height > 10000)
+        custom_game_menu.height = 0;
       break;
     case 2:
-      menu->mines--;
-      if (menu->mines > 100000)
-        menu->mines = 0;
+      custom_game_menu.mines--;
+      if (custom_game_menu.mines > 100000)
+        custom_game_menu.mines = 0;
       break;
     default:
       break;
@@ -103,20 +104,17 @@ char custom_game_control(CustomGameMenu *menu) {
   }
   return 0;
 }
+
 GameInstance get_custom_game(unsigned terminal_width,
                              unsigned terminal_height) {
-  CustomGameMenu menu;
-  menu.width = 40;
-  menu.height = 10;
-  menu.mines = 40;
-  menu.cursor_position = 0;
   int ret = 0;
   while (!ret) {
     erase();
-    cm_print(&menu, terminal_width, terminal_height);
-    ret = custom_game_control(&menu);
+    cm_print(terminal_width, terminal_height);
+    ret = custom_game_control();
     if (ret == -1)
       return select_mode(terminal_width, terminal_height);
   }
-  return createGameInstance(menu.width, menu.height, menu.mines);
+  return createGameInstance(custom_game_menu.width, custom_game_menu.height,
+                            custom_game_menu.mines);
 }
